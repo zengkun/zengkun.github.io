753918d4519b0b0d82b34bffddf37e3c74673331
zengkun <zengkun0415@sina.com>
2017-04-07 14:18:00 +0800

    add test

--- vnote/static/data.js
+++ vnote/static/data.js
@@ -1,12 +1,9 @@
-export default {
-  items: [{
-    title:"Vue路由的使用",
-    router:"router"
-  },{
-    title:"vue组件开发",
-    router:"component"
-  },{
-    title:"add test",
-    router:"test"
-  }]
+var data = {
+	items: [{
+		title: "Vue路由的使用",
+		router: "router"
+	}, {
+		title: "vue组件开发",
+		router: "component"
+	}]
 }
